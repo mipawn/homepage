b68b69466efbc5fc607605749d76cf486adb10f0
mipawn <admin@mipawn.local>
2020-04-24 10:24:42 +0800

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# blog后台管理系统
+
+## 声明
+
+本项目是基于开源项目vue-element-amdin进行的二次来发。感谢@PanJiaChen.（ https://github.com/PanJiaChen/vue-element-admin.git ）
+
+## 命令
+
+```sh
+本地开发 npm run serve
+
+commit 规范 angluar规范
+
+ps:  feat(app): init
+
+自动化构建 git push自动触发
+
+```
---
+++ src/views/markdown/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="components-container">
+    <!-- tui.editor -->
+    <header>
+      <div class="title">
+        <input type="text" placeholder="请输入文章标题">
+      </div>
+      <div class="submit">发布</div>
+    </header>
+    <section />
+    <div class="editor-container">
+      <markdown-editor ref="markdownEditor" v-model="content1" height="300px" :language="language" />
+    </div>
+    <el-button style="margin-top:80px;" type="primary" icon="el-icon-document" @click="getHtml">
+      Get HTML
+    </el-button>
+    <div v-html="html" />
+  </div>
+</template>
+
+<script>
+import MarkdownEditor from '@/components/MarkdownEditor'
+
+const content = `
+**This is test**
+
+* vue
+* element
+* webpack
+
+`
+export default {
+  name: 'Markdown',
+  components: { MarkdownEditor },
+  data() {
+    return {
+      content1: content,
+      content2: content,
+      content3: content,
+      content4: content,
+      html: '',
+      languageTypeList: {
+        'en': 'en_US',
+        'zh': 'zh_CN',
+        'es': 'es_ES'
+      }
+    }
+  },
+  computed: {
+    language() {
+      return this.languageTypeList['zh']
+    }
+  },
+  methods: {
+    getHtml() {
+      this.html = this.$refs.markdownEditor.getHtml()
+      console.log(this.html)
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.editor-container{
+  margin-bottom: 30px;
+}
+header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 100px;
+}
+.submit {
+  color: #1890ff;
+}
+</style>
